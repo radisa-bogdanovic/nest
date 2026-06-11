e38980a6526e34175d485375f133ba4fee3fb8b0
radisa-bogdanovic <tictac992@gmail.com>
2026-06-11 12:26:11 +0200

    dodan access token u cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -48,9 +48,16 @@ export class AuthController {
     res.cookie('refreshToken', tokens.refreshToken, {
       httpOnly: true,
       secure: true,
-      sameSite: 'lax',
-      path: 'auth/refresh',
+      sameSite: 'none',
+      path: '/auth/refresh',
       maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res.cookie('accessToken', tokens.accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+      path: '/',
+      maxAge: 15 * 60 * 1000,
     });
     return { accessToken: tokens.accessToken };
   }
